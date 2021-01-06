098c91038d881e1943c09552b510ea6a87f28d0b
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2021-01-05 23:32:59 -0500

    Remove Python 3.4 support

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'xlwt',
 		'joblib'
     ],
-	python_requires='>=3.4, <4',
+	python_requires='>=3.5, <4',
 	classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -37,10 +37,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
 		'Topic :: Scientific/Engineering :: Information Analysis',
