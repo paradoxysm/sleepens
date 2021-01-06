17bb39b2e7d546b8f476220de88ca4c7b251a599
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2021-01-05 23:38:04 -0500

    Return python 3.4 support

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'xlwt',
 		'joblib'
     ],
-	python_requires='>=3.5, <4',
+	python_requires='>=3.4, <4',
 	classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -37,6 +37,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
