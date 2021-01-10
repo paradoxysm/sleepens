ba0f5b9b0a964b8a8952dc0360ba69245d7db291
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2021-01-10 16:00:13 -0500

    Clean unused models

--- sleepens/ml/models/__init__.py
+++ sleepens/ml/models/__init__.py
@@ -1,19 +1,9 @@
-from ._nn import NeuralNetwork
 from ._gb import GradientBoostingClassifier
-from ._rf import RandomForestClassifier
-from ._lr import LogisticRegression
 from ._tsens import TimeSeriesEnsemble
-from ._dtsens import DynamicTimeSeriesEnsemble
-from ._des import DynamicEnsembleKNN
 from ._stsens import StackedTimeSeriesEnsemble
 
 __all__ = [
-			'NeuralNetwork',
 			'GradientBoostingClassifier',
-			'RandomForestClassifier',
-			'LogisticRegression',
 			'TimeSeriesEnsemble',
-			'DynamicTimeSeriesEnsemble',
-			'DynamicEnsembleKNN',
 			'StackedTimeSeriesEnsemble',
 		]
