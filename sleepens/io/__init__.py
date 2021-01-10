031d595f98ede5dcc4b95fbdea3f1fc54892dfcb
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2021-01-10 13:31:44 -0500

    Add check_reader

--- sleepens/io/__init__.py
+++ sleepens/io/__init__.py
@@ -1,9 +1,11 @@
 from ._batch_dataset import BatchDataset
 from ._dataset import Dataset
 from ._dataobject import DataObject
+from ._base_reader import check_reader
 
 __all__ = [
 			'BatchDataset',
 			'Dataset',
-			'DataObject'
+			'DataObject',
+			'check_reader'
 		]
