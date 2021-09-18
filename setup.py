d7d94cb9c6df4aa76aa93aaf79074471c5cecb63
Jeffrey <jeffreychen.wang@gmail.com>
2021-09-18 18:14:05 -0400

    Fix indentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 def read(*paths):
-    """
+	"""
 	Build a file path from *paths* and return the contents.
 	"""
 	with open(os.path.join(*paths), 'r') as f:
@@ -11,8 +11,8 @@ def read(*paths):
 setup(
 	name='sleepens',
 	version='1.0.0',
-    description='Sleep Classification using Ensemble Classification',
-    long_description=(read('README.md') + '\n\n'),
+	description='Sleep Classification using Ensemble Classification',
+	long_description=(read('README.md') + '\n\n'),
 	long_description_content_type="text/markdown",
 	url='http://github.com/paradoxysm/sleepens',
 	download_url = 'https://github.com/paradoxysm/sleepens/archive/1.0.0.tar.gz',
