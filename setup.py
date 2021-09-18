4b3ac4de72eec293f3e49a3a186146af7056d944
Jeffrey <jeffreychen.wang@gmail.com>
2021-09-18 00:00:33 -0400

    Update pkg setup

--- setup.py
+++ setup.py
@@ -5,22 +5,23 @@ def read(*paths):
     """
 	Build a file path from *paths* and return the contents.
 	"""
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
+	with open(os.path.join(*paths), 'r') as f:
+		return f.read()
 
 setup(
 	name='sleepens',
-	version='0.1.0',
+	version='1.0.0',
     description='Sleep Classification using Ensemble Classification',
     long_description=(read('README.md') + '\n\n'),
 	long_description_content_type="text/markdown",
 	url='http://github.com/paradoxysm/sleepens',
-	download_url = 'https://github.com/paradoxysm/sleepens/archive/0.1.0.tar.gz',
-    author='paradoxysm',
+	download_url = 'https://github.com/paradoxysm/sleepens/archive/1.0.0.tar.gz',
+	author='paradoxysm',
 	author_email='paradoxysm.dev@gmail.com',
-    license='BSD-3-Clause',
-    packages=find_packages(),
-    install_requires=[
+	license='BSD-3-Clause',
+	packages=find_packages(exclude=['tests']),
+	include_package_data=True,
+	install_requires=[
 		'numpy',
 		'sklearn',
 		'joblib',
@@ -28,24 +29,23 @@ setup(
 		'xlrd',
 		'xlwt',
 		'sonpy',
-    ],
-	python_requires='>=3.5, <4',
+	],
+	python_requires='>=3.7, <4',
 	classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
 		'Programming Language :: Python :: 3.7',
 		'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+		'Programming Language :: Python :: 3.9',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence',
 		'Topic :: Scientific/Engineering :: Information Analysis',
 		'Intended Audience :: Science/Research',
-    ],
+	],
 	keywords=['python', 'ml', 'ensemble', 'sleep', 'classification'],
-    zip_safe=True)
+	zip_safe=True)
