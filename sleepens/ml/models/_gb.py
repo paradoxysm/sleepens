5e0c4c813757be2b48836ad04f2c93f7564494dc
Jeffrey <jeffreychen.wang@gmail.com>
2021-11-17 11:20:41 -0500

    Remove min_impurity_split

--- sleepens/ml/models/_gb.py
+++ sleepens/ml/models/_gb.py
@@ -35,8 +35,8 @@ class GradientBoostingClassifier(Classifier):
         learners. If smaller than 1.0 this results in Stochastic Gradient
         Boosting. `subsample` interacts with the parameter `n_estimators`.
         Choosing `subsample < 1.0` leads to a reduction of variance
-
         and an increase in bias.
+
     criterion : {'friedman_mse', 'mse'}, default='friedman_mse'
         The function to measure the quality of a split. Supported criteria
         are 'friedman_mse' for the mean squared error with improvement
@@ -220,7 +220,7 @@ class GradientBoostingClassifier(Classifier):
 	def __init__(self, loss='deviance', learning_rate=0.1, n_estimators=100,
 				subsample=1.0, criterion='friedman_mse', min_samples_split=2,
 				min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3,
-				min_impurity_decrease=0.0, min_impurity_split=None, init=None,
+				min_impurity_decrease=0.0, init=None,
 				random_state=None, max_features=None, verbose=0, max_leaf_nodes=None,
 				warm_start=False, validation_fraction=0.1, n_iter_no_change=None,
 				tol=0.0001, ccp_alpha=0.0):
@@ -229,7 +229,7 @@ class GradientBoostingClassifier(Classifier):
 					subsample=subsample, criterion=criterion, min_samples_split=min_samples_split,
 					min_samples_leaf=min_samples_leaf, min_weight_fraction_leaf=min_weight_fraction_leaf,
 					max_depth=max_depth, min_impurity_decrease=min_impurity_decrease,
-					min_impurity_split=min_impurity_split, init=init, random_state=random_state,
+					init=init, random_state=random_state,
 					max_features=max_features, verbose=verbose, max_leaf_nodes=max_leaf_nodes,
 					warm_start=warm_start, validation_fraction=validation_fraction,
 					n_iter_no_change=n_iter_no_change, tol=tol, ccp_alpha=ccp_alpha)
